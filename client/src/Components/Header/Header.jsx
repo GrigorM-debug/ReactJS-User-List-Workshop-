406ba5e4ced6fd0c6b0450a2a36fa3d62f5923b5
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-07-07 14:50:41 +0300

    Some styles changes and added Search Controller with styles

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import styles from './Header.module.css';
+
 export default function Header() {
     return (
-        <header className="header">
-            <div className="logo">
-                <span className="course">React Workshop - September 2023</span>
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <span className={styles.course}>React Workshop - September 2023</span>
             </div>
         </header>
     );
